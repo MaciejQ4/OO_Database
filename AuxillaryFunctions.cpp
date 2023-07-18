7a2867aa5160c75293593bf9e4e0c8db89bbad56
Maciej Kubicz <maciejkubicz@protonmail.com>
2023-07-18 20:11:02 +0200

    added readline without executing

--- AuxillaryFunctions.cpp
+++ AuxillaryFunctions.cpp
@@ -1,1 +1,13 @@
 #include "AuxillaryFuntions.h"
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+string AuxillaryFuntions::readLine() {
+
+    cin.ignore();
+    string input;
+    getline(cin, input);
+    return input;
+}
--- AuxillaryFuntions.h
+++ AuxillaryFuntions.h
@@ -2,9 +2,18 @@
 
 #ifndef AUXILLARYFUNCTIONS_H
 #define AUXILLARYFUNCTIONS_H
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
+using namespace std;
 
 class AuxillaryFuntions
 {
+public:
+
+	string readLine();
 };
 
 #endif
